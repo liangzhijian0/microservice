e30886e635f8b0edd98f5e91ef5963f90816fb38
Ocean Liang <ocean.liang@cargosmart.com>
2019-03-20 12:34:57 +0800

    set gateway accessFilter

--- api-gateway/src/main/java/com/apigateway/ApiGatewayApplication.java
+++ api-gateway/src/main/java/com/apigateway/ApiGatewayApplication.java
@@ -3,6 +3,7 @@ package com.apigateway;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Bean;
 
 @EnableZuulProxy
 @SpringBootApplication
@@ -12,4 +13,8 @@ public class ApiGatewayApplication {
 		SpringApplication.run(ApiGatewayApplication.class, args);
 	}
 
+    @Bean
+    public AccessFilter accessFilter() {
+        return new AccessFilter();
+    }
 }
